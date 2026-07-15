94d629d2d84798ef2bdaf128da5c6d10b2410eed
Saniya7277 <saniyaariha143@gmail.com>
2026-07-15 22:43:21 +0530

    Add secondLargestElement method in Solution class
    Implement a method to find the second largest element in an array, handling negative values.

--- Arrays/secondLargest.java
+++ Arrays/secondLargest.java
@@ -25,3 +25,42 @@ class Solution {
     
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//The more efficient approach in case of negative elements
+
+class Solution {
+    public int secondLargestElement(int[] nums) {
+
+        int max = Integer.MIN_VALUE;
+        int secondmax = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums.length; i++) {
+
+            if(nums[i] > max) {
+                secondmax = max;
+                max = nums[i];
+            }
+            else if(nums[i] > secondmax && nums[i] != max) {
+                secondmax = nums[i];
+            }
+        }
+
+        if(secondmax == Integer.MIN_VALUE) {
+            return -1;
+        }
+
+        return secondmax;
+    }
+}
