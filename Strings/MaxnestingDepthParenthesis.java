054eb1d66844d6a4979c362afd42b5dc2e7a2d33
Saniya7277 <saniyaariha143@gmail.com>
2026-08-05 23:36:05 +0530

    Implement maxDepth method for parentheses

---
+++ Strings/MaxnestingDepthParenthesis.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count = 0;
+        int maxDepth = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+
+                count++;
+
+                if (count > maxDepth) {
+                    maxDepth = count;
+                }
+
+            } else if (ch == ')') {
+
+                count--;
+
+            }
+        }
+        return maxDepth;
+        
+    }
+}
