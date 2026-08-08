625c6750e71cba1b18e343afe50f1c5b9121ecef
Saniya7277 <saniyaariha143@gmail.com>
2026-08-08 23:40:20 +0530

    Implement deleteMiddle function for linked list

---
+++ LinkedList/deleteMiddle.java
@@ -0,0 +1,45 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Delete the middle node
+        prev.next = slow.next;
+
+        return head;
+    }
+    }
