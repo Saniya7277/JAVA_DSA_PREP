9aa7f7bd52b85d4fc8107bf119d555d8441d5e80
Saniya7277 <saniyaariha143@gmail.com>
2026-08-08 21:48:02 +0530

    Add method to segregate odd and even nodes
    Implement segregation of odd and even nodes in a linked list.

---
+++ LinkedList/SegregateEvenOdd.java
@@ -0,0 +1,38 @@
+/*
+Definition of singly linked list:
+class ListNode{
+    public int data;
+    public ListNode next;
+    ListNode() { data = 0; next = null; }
+    ListNode(int x) { data = x; next = null; }
+    ListNode(int x, ListNode next) { data = x; this.next = next; }
+}
+*/
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        //YOUR CODE GOES HERE
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
+
+    }
+}
